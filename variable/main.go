da28452e4887fc466453ef36b1207b85931cae64
li.zhuang <875626439@qq.com>
2020-01-26 23:51:21 +0800

    variable example modeify

--- variable/main.go
+++ variable/main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	var str string = "string"
 	fmt.Println(str)
-	var a, b int = 12, 13
+	var a, b int = 12, 1
 	fmt.Println(a, b)
+	fmt.Println(&a, &b)
+	autoType := "auto type"
+	fmt.Println(autoType)
 }
